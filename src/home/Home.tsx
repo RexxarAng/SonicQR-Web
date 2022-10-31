c26890ab6d63cdc6a9f6bcd73665c391631b9025
RexxarAng <rexar.aa@gmail.com>
2022-11-01 00:22:03 +0800

    Updated text for home page and remove receive tab

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -5,8 +5,8 @@ import './Home.css';
 class Home extends React.Component {
   render() {
     return <Container maxWidth="sm">
-      <h3>Home</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h1>Home</h1>
+      <h3>SonicQR allows uploading of file to convert to base64 before encoding to a series of animating QR codes to be scanned</h3>
     </Container>;
   }
 }
